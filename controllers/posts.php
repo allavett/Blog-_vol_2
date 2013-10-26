fff8b3800c524f7a90f60915a7c2b0efb84454d6
kemo <kemo.oolep@gmail.com>
2013-10-26 13:27:37 +0300

    Post index query

--- controllers/posts.php
+++ controllers/posts.php
@@ -3,6 +3,6 @@
 class posts extends Controller{
 
 	function index(){
-		$this->users = get_all("SELECT * FROM user");
+        $this->posts = get_all("SELECT * FROM post NATURAL JOIN user ORDER BY post_created DESC");
 	}
 }
