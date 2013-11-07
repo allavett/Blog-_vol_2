9a1c1735dc6c09c4ea3bdaaed7401142da10a1f4
Allar Vendla <allar.vendla@khk.ee>
2013-11-07 23:21:08 +0200

    Add more checks to register info

--- controllers/register.php
+++ controllers/register.php
@@ -6,6 +6,8 @@
  * Time: 17:42
  * To change this template use File | Settings | File Templates.
  */
+/** This controller checks all the data submitted by user on the register form.
+ * If there are any incoherence in data, user will get a notification about what went wrong.*/
 
 class register extends Controller {
 	function index (){
@@ -16,39 +18,46 @@ class register extends Controller {
 			$usernames=get_all("SELECT username FROM user");
 			foreach ($usernames as $username){
 				$this->usernames[$username['username']][]= array('username'=>$username['username']);
-				if ($_POST['username']== $username['username']){
+				if (strtolower($_POST['username'])== strtolower($username['username'])){
 					$this->notification = '<span class="label" style="background-color:#ff5849">Username already taken!</span>';
+					return;
+				}elseif(preg_match('/[\s-_!@£$€})({"#¤%&=?]+/', $_POST['username'])){
+					$this->notification = '<span class="label" style="background-color:#ff5849">Username can contain only letters and numbers!</span>';
 					return;
 				}
 			}
-			if (preg_match("/@/", $_POST['email'])) {
-				if ($_POST['password']==$_POST['password_confirm']){
-					$allowedExts = array("gif", "jpeg", "jpg", "png");
-					$temp = explode(".", $_FILES["file"]["name"]);
-					$extension = end($temp);
-					if (($_FILES["file"]["size"] < 100000)&& in_array($extension, $allowedExts)){
-						$tmp_file=$_FILES["file"]["tmp_name"];
-						$img_dimensions = getimagesize($tmp_file);
-						$width = $img_dimensions[0];
-						$height = $img_dimensions[1];
-						if ($width > 200||$height > 100) {
-							$this->notification = '<span class="label" style="background-color:#ff5849">Image dimensions too big!</span>';
+			if (preg_match("/@/", $_POST['email'])&&!preg_match("/ /", $_POST['email'])) {
+				if ($_POST['password']>=4){
+					if ($_POST['password']==$_POST['password_confirm']){
+						$allowedExts = array("gif", "jpeg", "jpg", "png");
+						$temp = explode(".", $_FILES["file"]["name"]);
+						$extension = end($temp);
+						if (($_FILES["file"]["size"] < 100000)&& in_array($extension, $allowedExts)){
+							$tmp_file=$_FILES["file"]["tmp_name"];
+							$img_dimensions = getimagesize($tmp_file);
+							$width = $img_dimensions[0];
+							$height = $img_dimensions[1];
+							if ($width > 200||$height > 100) {
+								$this->notification = '<span class="label" style="background-color:#ff5849">Image dimensions too big!</span>';
+							}else{
+								if (!is_dir('upload/')) {
+									mkdir('upload/', 0777, true);
+								}
+								$avatar=$_POST['username'].$_FILES["file"]["name"];
+								move_uploaded_file($tmp_file,"upload/".$avatar);
+								$sql_user = insert('user', array('username' => $_POST['username'], 'password' =>$_POST['password'], 'email' =>$_POST['email'],'avatar'=>$avatar));
+								if($sql_user){
+									$this->auth->redirect(DEFAULT_CONTROLLER);
+								}
+							}
 						}else{
-							if (!is_dir('upload/')) {
-								mkdir('upload/', 0777, true);
-							}
-							$avatar=$_POST['username'].$_FILES["file"]["name"];
-							move_uploaded_file($tmp_file,"upload/".$avatar);
-							$sql_user = insert('user', array('username' => $_POST['username'], 'password' =>$_POST['password'], 'email' =>$_POST['email'],'avatar'=>$avatar));
-							if($sql_user){
-								$this->auth->redirect(DEFAULT_CONTROLLER);
-							}
+							$this->notification = '<span class="label" style="background-color:#ff5849">Check your image file!</span>';
 						}
 					}else{
-						$this->notification = '<span class="label" style="background-color:#ff5849">Check your image file!</span>';
+						$this->notification = '<span class="label" style="background-color:#ff5849">Password does not match!</span>';
 					}
 				}else{
-					$this->notification = '<span class="label" style="background-color:#ff5849">Password does not match!</span>';
+					$this->notification = '<span class="label" style="background-color:#ff5849">Password is too short!</span>';
 				}
 			}else{
 				$this->notification = '<span class="label" style="background-color:#ff5849">Not a valid email address!</span>';
